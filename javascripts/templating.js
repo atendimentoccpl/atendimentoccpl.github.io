7d02c1977b26ff38ec34aa749e16b652e020c822
atendimentoccpl <atendimentoccpl@gmail.com>
2015-10-16 10:12:33 -0300

    Update templating.js

--- javascripts/templating.js
+++ javascripts/templating.js
@@ -26,3 +26,16 @@ function generateTemplate(selector){
     }
     else throw Error("Type of '"+selector+"' is not a template/html");
 }
+
+if (!String.prototype.formatWithObj) {
+    String.prototype.formatWithObj = function() {
+        var str = this.toString();
+        if (!arguments.length)
+            return str;
+        var args = typeof arguments[0],
+            args = (("string" == args || "number" == args) ? arguments : arguments[0]);
+        for (arg in args)
+            str = str.replace(RegExp("\\{" + arg + "\\}", "gi"), args[arg]);
+        return str;
+    }
+}
